94fc8b71afc4719276168b2b5573b45c8b6620da
Michael Harrison <mh@michaelharrison.ws>
2012-02-23 17:01:09 +0000

    "Adds a check for C9's ENV var for port"

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
-port = process.env.PORT || process.env.PORT || 3000;
+
+port = process.env.C9_PORT || process.env.PORT || 3000;
 app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
