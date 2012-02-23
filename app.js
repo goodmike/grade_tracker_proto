7282a752569411d842087670e4bad412d7183dd2
Michael Harrison <mh@michaelharrison.ws>
2012-02-23 10:58:03 -0600

    Set port by ENV var, default to 3000
    process.env.PORT or 3000

--- app.js
+++ app.js
@@ -30,6 +30,6 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
-
-app.listen(3000);
+port = process.env.PORT || process.env.PORT || 3000;
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
