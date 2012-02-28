81263ae0cb609bb5f1e6165901230055c1298bc0
Michael Harrison <mh@michaelharrison.ws>
2012-02-28 17:59:36 +0000

    "Corrects bug in show view"

--- routes/show.js
+++ routes/show.js
@@ -1,14 +1,10 @@
-
 /*
  * GET tracker display.
  */
 
-exports.index = function(db) {
+exports.show = function(db) {
     return function(req,res) {
         db.get('_design/basic/_view/scores', function(err,doc) {
-            for (var key in err) {
-                console.log("err[" + key + "]: " + err[key]);
-            }
            res.render('show', {
               title: 'Grade Tracker',
               items: doc
