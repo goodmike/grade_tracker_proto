99a791eb35e65911f1a5c86ffa44649239f57482
Michael Harrison <mh@michaelharrison.ws>
2012-03-09 21:45:43 +0000

    "Changes UI to table, explores render and event options"

--- docs/journal.md
+++ docs/journal.md
@@ -5,3 +5,13 @@
 * Following examples in Hypermedia book, set up tempates for the server view.
 
 * I could make an alternate template that puts data on page to bootstrap it.
+
+### Using the HTML API
+
+* Let's provide a method to GradeList.parse that takes the table and prepares it for display
+  * The models could be much more interesting, more geared to chart display.
+  * The collection could represent chart, could contain data table as element.
+* Step 1: JS that takes table and makes graph
+  * First into JS data structure. 
+  * Then, prepare jqlot-ready data (view stage)
+  * Then, apply charting method to view data.
--- public/javascripts/backbone_app.js
+++ public/javascripts/backbone_app.js
@@ -1,3 +1,16 @@
+$("#grades_chart").on('gradesRedraw', function() {
+   alert("Redraw!"); 
+});
+
+var tableHeaders = function() {
+    var headers = [].slice.apply(arguments);
+    return "<tr>" + 
+        headers.map(function(hdr) { 
+            return "<th>" + hdr + "</th>"
+        }).join("\n") + 
+    "</tr>";
+};
+
 $(function() {
    
    window.Grade = Backbone.Model.extend({
@@ -10,31 +23,37 @@ $(function() {
        url: "/grades",
        parse: function(response) {
            var items = new Backbone.Collection(response.collection.items);
+           this.items_length = items.length;
            return items.pluck("data");
        }
    });
-//   window.Grades = new GradeList;
 
     window.GradeListView = Backbone.View.extend({
  
-        tagName:'div',
+        tagName:'table',
  
         initialize:function () {
             this.model.bind("reset", this.render, this);
         },
  
         render:function (eventName) {
+            if (this.model.items_length) {
+                $("#specialoutput").html("" + this.model.items_length + " records");
+            }
+            $(this.el).html(tableHeaders("Date","Assessment","Score","Weighted Score"));
             _.each(this.model.models, function (grade) {
                 $(this.el).append(new GradeView({model:grade}).render().el);
             }, this);
+            $.event.trigger('gradesRedraw');
             return this;
         }
+        
     });
    
    window.GradeView = Backbone.View.extend({
       
-      tagName: 'p',
-      template: _.template("score: <%= score %>; assessment: <%= assessment %>"),
+      tagName: 'tr',
+      template: _.template("<td><%= score %></td><td><%= assessment %></td><td><%= score %></td><td><%= weighted_score %></td>"),
       
       render: function() {
         console.log("GradeView render");
@@ -45,7 +64,7 @@ $(function() {
    
    window.AppView = Backbone.View.extend({
       
-        el: $("body"),
+        el: $("#grades_table"),
         initialize: function() {
             
             this.grades = new GradeList();
