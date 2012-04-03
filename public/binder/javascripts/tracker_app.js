3e4e328aa3ce5ff6fec185f83bb7d1af1e0558b0
Michael Harrison <mh@michaelharrison.ws>
2012-04-03 15:16:40 -0500

    silence add after create grade

--- public/binder/javascripts/tracker_app.js
+++ public/binder/javascripts/tracker_app.js
@@ -375,7 +375,7 @@ $(function() {
             );
             if (this.model.isNew()) {
                 app.grades.create(this.model, {
-                    wait: true,
+                    silent: true,
                     success: function(model, response) {
                         var weighted_score = model.attributes.score; // will come from response
                         model.set({weighted_score: model.attributes.score});
